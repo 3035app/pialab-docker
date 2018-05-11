5f9224dd3956f8bae7d921db5a1abe9376ea4248
Fanch <fanch.gerbes@gmail.com>
2018-05-11 17:28:00 +0200

    [CI] Uppercase Name

--- install.sh
+++ install.sh
@@ -4,21 +4,21 @@ set -ex
 
 
 
-if [ -z "${Name}" ]
+if [ -z "${NAME}" ]
 then
-    Name=pialab
+    NAME=pialab
 fi
 
 mkdir -p $(pwd)/var
 
 if [ -z "${ETCDDATA}" ]
 then
-    ETCDDATA="$(pwd)/var/${Name}-etcd-data"
+    ETCDDATA="$(pwd)/var/${NAME}-etcd-data"
 fi
 
 if [ -z "${POSTGRESDATA}" ]
 then
-    POSTGRESDATA="$(pwd)/var/${Name}-postgres-data"
+    POSTGRESDATA="$(pwd)/var/${NAME}-postgres-data"
 fi
 
 if [ -z "${SERVERPORT}" ]
@@ -36,20 +36,25 @@ then
     BACKURL="http://localhost:${SERVERPORT}/back"
 fi
 
+if [ -z "${CREATEUSER}" ]
+then
+    CREATEUSER=true
+fi
+
 # clean
-docker rm -f etcd.${Name}.cnt || echo "Ok"
-docker rm -f postgresql.${Name}.cnt || echo "Ok"
-docker rm -f apache.${Name}.cnt || echo "Ok"
-docker network rm ${Name}.network || echo "Ok"
+docker rm -f etcd.${NAME}.cnt || echo "Ok"
+docker rm -f postgresql.${NAME}.cnt || echo "Ok"
+docker rm -f apache.${NAME}.cnt || echo "Ok"
+docker network rm ${NAME}.network || echo "Ok"
 
 # create network
-docker network create ${Name}.network
+docker network create ${NAME}.network
 
 # install etcd
 REGISTRY=quay.io/coreos/etcd
-docker run -dt --network=${Name}.network \
+docker run -dt --network=${NAME}.network \
        --volume ${ETCDDATA}:/etcd-data \
-       --name etcd.${Name}.cnt ${REGISTRY}:v3.1.1 \
+       --name etcd.${NAME}.cnt ${REGISTRY}:v3.1.1 \
        /usr/local/bin/etcd \
        --data-dir=/etcd-data --name node1 \
        --initial-advertise-peer-urls http://0.0.0.0:2380 \
@@ -57,25 +62,26 @@ docker run -dt --network=${Name}.network \
        --advertise-client-urls http://0.0.0.0:2379 \
        --listen-client-urls http://0.0.0.0:2379
 
-ETCDHOST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' etcd.${Name}.cnt)
+ETCDHOST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' etcd.${NAME}.cnt)
 
 # install postgres
 DBROOTUSER=postgres
 DBROOTPASSWORD=postgres24
-docker run -dt --network=${Name}.network \
+docker run -dt --network=${NAME}.network \
        --volume ${POSTGRESDATA}:/var/lib/postgresql/data \
        -e POSTGRES_PASSWORD=${DBROOTPASSWORD} \
-       --name postgresql.${Name}.cnt postgres:9.6 
+       --name postgresql.${NAME}.cnt postgres:9.6 
 
-DBHOST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgresql.${Name}.cnt)
+DBHOST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgresql.${NAME}.cnt)
 
 echo 'var' > .dockerignore
 
-# install ${Name}
-docker rm -f apache.${Name}.cnt|| echo "Ok"
-docker -D build --network=${Name}.network --build-arg CACHEBUST=$(shuf -n 1 -i 100-1000) \
+# install ${NAME}
+docker rm -f apache.${NAME}.cnt|| echo "Ok"
+docker -D build --network=${NAME}.network --build-arg CACHEBUST=$(shuf -n 1 -i 100-1000) \
        --build-arg DBHOST=${DBHOST} --build-arg ETCDHOST=${ETCDHOST} \
        --build-arg FRONTURL=${FRONTURL} --build-arg BACKURL=${BACKURL} \
-       -f Dockerfile -t ${Name}.img .
-docker run -dt --network=${Name}.network -p ${SERVERPORT}:80 --name apache.${Name}.cnt ${Name}.img
+       --build-arg CREATEUSER=${CREATEUSER} \
+       -f Dockerfile -t ${NAME}.img .
+docker run -dt --network=${NAME}.network -p ${SERVERPORT}:80 --name apache.${NAME}.cnt ${NAME}.img
 
